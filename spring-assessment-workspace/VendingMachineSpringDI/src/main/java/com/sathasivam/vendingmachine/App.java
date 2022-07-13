e77148f8909515ef95e7b0c394259000b28b8675
Satha Sivam <satha.sivam@outlook.com>
2022-07-13 16:44:35 +0800

    Spring DI

--- spring-assessment-workspace/VendingMachineSpringDI/src/main/java/com/sathasivam/vendingmachine/App.java
+++ spring-assessment-workspace/VendingMachineSpringDI/src/main/java/com/sathasivam/vendingmachine/App.java
@@ -1,29 +1,29 @@
 package com.sathasivam.vendingmachine;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.sathasivam.vendingmachine.controller.VendingMachineController;
-import com.sathasivam.vendingmachine.dao.VendingMachineAuditDao;
-import com.sathasivam.vendingmachine.dao.VendingMachineAuditDaoFileImpl;
-import com.sathasivam.vendingmachine.dao.VendingMachineDao;
-import com.sathasivam.vendingmachine.dao.VendingMachineDaoFileImpl;
-import com.sathasivam.vendingmachine.service.VendingMachineServiceLayer;
-import com.sathasivam.vendingmachine.service.VendingMachineServiceLayerImpl;
-import com.sathasivam.vendingmachine.ui.UserIO;
-import com.sathasivam.vendingmachine.ui.UserIOConsoleImpl;
-import com.sathasivam.vendingmachine.ui.VendingMachineView;
+
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		UserIO io = new UserIOConsoleImpl();
-        VendingMachineView view = new VendingMachineView(io);
-        VendingMachineAuditDao auditDao = new VendingMachineAuditDaoFileImpl();
-        VendingMachineDao dao = new VendingMachineDaoFileImpl();
-        VendingMachineServiceLayer service = new VendingMachineServiceLayerImpl(auditDao, dao);
-        VendingMachineController controller = new VendingMachineController(view, service);
-        
-        controller.run();
+//		UserIO io = new UserIOConsoleImpl();
+//        VendingMachineView view = new VendingMachineView(io);
+//        VendingMachineAuditDao auditDao = new VendingMachineAuditDaoFileImpl();
+//        VendingMachineDao dao = new VendingMachineDaoFileImpl();
+//        VendingMachineServiceLayer service = new VendingMachineServiceLayerImpl(auditDao, dao);
+//        VendingMachineController controller = new VendingMachineController(view, service);
+//        
+//        controller.run();
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		VendingMachineController controller = ctx.getBean("controller", VendingMachineController.class);
+		controller.run();
+		
 	}
 
 }
